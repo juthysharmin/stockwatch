4de4646707d91e300de890c20e5232afc24cebc8
mohammedshakilgit <shakilxps@gmail.com>
2022-09-15 20:03:53 -0400

    Sept 15

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,27 +1,10 @@
 package Pages;
-<<<<<<< HEAD
-=======
 
->>>>>>> main
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-<<<<<<< HEAD
 	
-	WebDriver driver;
-public LoginPage(WebDriver driver) {
-		this.driver=driver;		
-		}
-	
-    public void login(String userName , String password) {
-	driver.findElement(By.id("ContentPlaceHolder1_txtUserName")).sendKeys(userName);
-	driver.findElement(By.id("ContentPlaceHolder1_txtPassword")).sendKeys(password);
-	driver.findElement(By.id("ContentPlaceHolder1_btnLogin")).click();
-	}
-}
-=======
-
 	WebDriver driver;
 
 	public LoginPage(WebDriver driver) {
@@ -34,4 +17,3 @@ public LoginPage(WebDriver driver) {
 		driver.findElement(By.id("ContentPlaceHolder1_btnLogin")).click();
 	}
 }
->>>>>>> main
--- src/test/java/NewTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-
-public class NewTest {
-  @Test
-  public void abcd() { {
-	  System.out.println("New Test");
-  }
-}
